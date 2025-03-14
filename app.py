b961ef0fd1a99e17ec5584f8600c9539d20299e4
HarshPatel8279 <hhenu8279@gmail.com>
2025-03-15 00:37:13 +0530

    added new route for user

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ def hello_world():
     return "<h1>Hello World!! I am Flask. </h1>"
 
 
+@app.route("/user/<username>")
+def hello_user(username):
+    return f"<h1>Hello, {username}! Flask this side.</h1>"
+
 if __name__ ==  "__main__":
     app.run(debug=True)
